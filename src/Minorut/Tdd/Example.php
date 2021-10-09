8932b505f4e4ccc0b49635b59c94442cddde40f8
minorut <minorut@users.noreply.github.com>
2021-10-09 20:46:58 +0900

    First commit

---
+++ src/Minorut/Tdd/Example.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Minorut\Tdd;
+
+class Example
+{
+    public function hello(): string
+    {
+        return 'Hello';
+    }
+}
---
+++ tests/Unit/Minorut/Tdd/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit\Minorut\Tdd;
+
+use Minorut\Tdd\Example;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @test
+     */
+    public function it_returns_hello(): void
+    {
+        $this->assertSame((new Example())->hello(), 'Hello');
+    }
+}
